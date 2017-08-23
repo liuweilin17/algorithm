e437f24a9ba6429955e030ea8f8f42345b948de4
liuwl <liuweilin17@qq.com>
2017-08-23 23:53:19 +0800

    fewfew

---
+++ interview/netease3.cpp
@@ -0,0 +1,29 @@
+//查找一个向右移动若干位的递增数组的最小值
+
+#include<iostream>
+using namespace std;
+int smallSearch(int* a, int n){
+    int left = 0, right = n-1, mid;
+    int ret;
+    while(left <= right){
+        mid = (right - left) / 2 + left;
+        if(a[mid] > a[left]){
+            left = mid + 1;
+        }else if(a[mid] < a[left]){
+            right = mid - 1;
+        }else{
+            ret = min(a[left], a[left+1]);
+            break;
+        }
+    }
+    return min(ret, a[0]);//有可能是完全递增数组，因此要把a[0]考虑进来
+    return -1;
+}
+int main(){
+    int a[] = {4,5,1,2,3};
+    int b[] = {1,2,3,4};
+
+    cout<<smallSearch(a, 4)<<endl;
+    cout<<smallSearch(b, 3)<<endl;
+    return 0;
+}
