6c9509c656b3f9744af21535c5abaf9ea99c4947
liuweilin17@qq.com <liuweilin17@qq.com>
2018-11-12 00:02:55 -0500

    modify 169.py

--- leetcode/169.py
+++ leetcode/169.py
@@ -27,7 +27,7 @@ class Solution(object):
                     count += 1
                 else:
                     count -= 1
-                if count <= 0:
+                if count <= 0: # notice
                     num = None
                     count = 0
             #print num, count
@@ -35,8 +35,8 @@ class Solution(object):
 
 if __name__ == '__main__':
     s = Solution()
-    print s.majorityElement([3,2,3])
-    print s.majorityElement([2,2,1,1,1,2,2])
-    print s.majorityElement([3,3,4])
+    print(s.majorityElement([3,2,3]))
+    print(s.majorityElement([2,2,1,1,1,2,2]))
+    print(s.majorityElement([3,3,4]))
     case1 = [47,47,72,47,72,47,79,47,12,92,13,47,47,83,33,15,18,47,47,47,47,64,47,65,47,47,47,47,70,47,47,55,47,15,60,47,47,47,47,47,46,30,58,59,47,47,47,47,47,90,64,37,20,47,100,84,47,47,47,47,47,89,47,36,47,60,47,18,47,34,47,47,47,47,47,22,47,54,30,11,47,47,86,47,55,40,49,34,19,67,16,47,36,47,41,19,80,47,47,27]
-    print s.majorityElement(case1)
+    print(s.majorityElement(case1))
