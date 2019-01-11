69d9e03d2bfee71dfe8a4cd1a9a34bfa9944ad43
liuweilin17@qq.com <liuweilin17@qq.com>
2019-01-10 20:16:13 -0500

    add combinations method to 46.py

--- leetcode/46.py
+++ leetcode/46.py
@@ -36,19 +36,33 @@ class Solution:
     # https://leetcode.com/problems/permutations/discuss/18239/A-general-approach-to-backtracking-questions-in-Java-(Subsets-Permutations-Combination-Sum-Palindrome-Partioning)
     def permute3(self, nums):
         ret = []
-        self.backtrack(ret, [], nums)
+        self.backtrack1(ret, [], nums)
         return ret
 
-    def backtrack(self, ret, tmp, nums):
+    def backtrack1(self, ret, tmp, nums):
         if len(tmp) == len(nums):
             ret.append(tmp[:])
         else:
             for n in nums:
                 if n not in tmp:
                     tmp.append(n)
-                    self.backtrack(ret, tmp[:], nums)
+                    self.backtrack(ret, tmp, nums)
                     tmp.pop()
 
+    def combine(self, nums, m):
+        ret = []
+        self.backtrack2(nums, m, 0, [], ret)
+        return ret
+
+    def backtrack2(self, nums, m, start, tmp, ret):
+        if len(tmp) == m:
+            ret.append(tmp[:])
+            return
+        for i in range(start, len(nums)):
+            if nums[i] not in tmp:
+                tmp.append(nums[i])
+                self.backtrack2(nums, m, i+1, tmp, ret)
+                tmp.pop()
 
 if __name__ == '__main__':
     s = Solution()
