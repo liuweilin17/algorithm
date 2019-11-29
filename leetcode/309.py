39f71249a5d8a9399bf17e9c16ca2e059bbb8f94
liuweilin17@qq.com <liuweilin17@qq.com>
2019-11-29 10:08:44 -0500

    modify 309

--- leetcode/309.py
+++ leetcode/309.py
@@ -11,26 +11,42 @@
 #309. Best Time to Buy and Sell Stock with Cooldown
 
 class Solution:
-    def maxProfit(self, prices: List[int]) -> int:
-        # buy[i] maximum profit which end with buying on day i or end with buying on a day before i and takes rest until
-        # the day i since then
-        # sell[i] maximum profit which end with selling on day i or end with selling on a day before i and takes rest
-        # until the day i since then
-        # buy[i] = max(buy[i-1], sell[i-2]-prices[i])
-        # sell[i] = max(sell[i-1], buy[i-1]+prices[i])
-        # b0 = max(b1, s2-prices[i])
-        # s0 = max(s1, b1+prices[i])
-
+    def maxProfit1(self, prices: List[int]) -> int:
+        # buy[i], the maximum profit of 0...i and last op is buy
+        # sell[i], the maximum profit of 0...i and last op is sell
         N = len(prices)
-        if N == 0 or N ==1: return 0
+        if N <= 1: return 0
+        elif N == 2: return max(0, prices[1]-prices[0])
+        else:
+            buy = [0 for _ in range(N)]
+            sell = [0 for _ in range(N)]
+            # Notice the initial value of buy and sell
+            buy[0] = -prices[0]
+            buy[1] = max(-prices[1], -prices[0])
+            sell[1] = max(prices[1] - prices[0], 0)
+            
+            for i in range(2, N):
+                buy[i] = max(buy[i-1], sell[i-2]-prices[i])
+                sell[i] = max(sell[i-1], buy[i-1]+prices[i])
+            return max(buy[N-1], sell[N-1])
 
-        b0, b1 = 0, -prices[0]
-        s0, s1, s2 = 0, 0, 0
-        for i in range(1, N):
-            b0 = max(b1, s2-prices[i])
-            s0 = max(s1, b1+prices[i])
-            b1 = b0
-            s2 = s1
-            s1 = s0
+    def maxProfit2(self, prices: List[int]) -> int:
+        # buy[i], the maximum profit of 0...i and last op is buy
+        # sell[i], the maximum profit of 0...i and last op is sell
+        N = len(prices)
+        if N <= 1: return 0
+        elif N == 2: return max(0, prices[1]-prices[0])
+        else:
+            # Notice the initial value of buy and sell
+            b0 = -prices[0]
+            b1 = max(-prices[1], -prices[0])
+            s0 = 0
+            s1 = max(prices[1] - prices[0], 0)
 
-        return s0
+            for i in range(2, N):
+                b2 = max(b1, s0-prices[i])
+                s2 = max(s1, b1+prices[i])
+                b1 = b2
+                s0 = s1
+                s1 = s2
+            return s2
