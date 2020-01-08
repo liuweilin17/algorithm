19a014234328d96e8914d323d2d75aec8b08ae5a
liuweilin17@qq.com <liuweilin17@qq.com>
2020-01-07 19:53:21 -0500

    modify question 79

--- leetcode/79.py
+++ leetcode/79.py
@@ -45,4 +45,35 @@ class Solution:
                     if backtrack(N, M, track + [(i, j)], word[1:]):
                         return True
         return False
+    
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        # DFS
+        visited = [] # the index of used grid
+        N = len(board)
+        if not N: return False
+        M = len(board[0])
+        dx = [0, 0, 1, -1]
+        dy = [1, -1, 0, 0]
+
+        def dfs(word, x, y): # whether word could be matched beginning with (x,y)
+            if not word:
+                return True
+            elif word[0] != board[x][y]:
+                return False
+            else:
+                visited.append((x, y))
+                for k in range(4):
+                    xx, yy = x + dx[k], y + dy[k]
+                    if (xx, yy) not in visited and 0<=xx<N and 0<=yy<M:
+                        if dfs(word[1:], xx, yy):
+                            return True
+                visited.pop()
+                return True if not word[1:] else False
+
+        for i in range(N):
+            for j in range(M):
+                if dfs(word, i, j):
+                    return True
+        return False
+
 
